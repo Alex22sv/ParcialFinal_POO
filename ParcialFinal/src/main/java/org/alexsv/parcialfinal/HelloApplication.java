991720c222aba34f74179de414f003a29071ec72
AlejandroFV74 <AlejandroFV74>
2024-07-05 19:23:54 -0600

    First prototype of purchases-made-screen

--- ParcialFinal/src/main/java/org/alexsv/parcialfinal/HelloApplication.java
+++ ParcialFinal/src/main/java/org/alexsv/parcialfinal/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("welcome-screen.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 500);
         stage.setTitle("Hello!");
         stage.setScene(scene);
