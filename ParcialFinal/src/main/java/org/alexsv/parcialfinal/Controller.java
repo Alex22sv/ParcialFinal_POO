526966298aeb1fb99105d1f59f2149e11a4f5338
Aisack23 <00016023@uca.edu.sv>
2024-07-06 20:58:37 -0600

    Adition of the Exit option

--- ParcialFinal/src/main/java/org/alexsv/parcialfinal/Controller.java
+++ ParcialFinal/src/main/java/org/alexsv/parcialfinal/Controller.java
@@ -1,5 +1,5 @@
 package org.alexsv.parcialfinal;
-
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -13,87 +13,57 @@ import java.io.IOException;
 public class Controller {
     @FXML
     private void openHome(ActionEvent event) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("welcome-screen.fxml"));
-        Parent root = fxmlLoader.load();
-        Scene scene = new Scene(root, 800, 500);
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setTitle("APP");
-        stage.setScene(scene);
-        stage.show();
+        changeScene(event, "welcome-screen.fxml", "APP");
     }
+
     @FXML
     private void openClient(ActionEvent event) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("client-screen.fxml"));
-        Parent root = fxmlLoader.load();
-        Scene scene = new Scene(root, 800, 500);
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setTitle("APP");
-        stage.setScene(scene);
-        stage.show();
+        changeScene(event, "client-screen.fxml", "APP");
     }
+
     @FXML
     private void openTransaction(ActionEvent event) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("transactions-screen.fxml"));
-        Parent root = fxmlLoader.load();
-        Scene scene = new Scene(root, 800, 500);
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setTitle("APP");
-        stage.setScene(scene);
-        stage.show();
+        changeScene(event, "transactions-screen.fxml", "APP");
     }
 
     @FXML
     private void openCards(ActionEvent event) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("cards-screen.fxml"));
-        Parent root = fxmlLoader.load();
-        Scene scene = new Scene(root, 800, 500);
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setTitle("APP");
-        stage.setScene(scene);
-        stage.show();
+        changeScene(event, "cards-screen.fxml", "APP");
     }
 
     @FXML
     private void openA(ActionEvent event) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("purchases-made-screen.fxml"));
-        Parent root = fxmlLoader.load();
-        Scene scene = new Scene(root, 800, 500);
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setTitle("APP");
-        stage.setScene(scene);
-        stage.show();
+        changeScene(event, "purchases-made-screen.fxml", "APP");
     }
+
     @FXML
     private void openB(ActionEvent event) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("money-spent-screen.fxml"));
-        Parent root = fxmlLoader.load();
-        Scene scene = new Scene(root, 800, 500);
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setTitle("APP");
-        stage.setScene(scene);
-        stage.show();
+        changeScene(event, "money-spent-screen.fxml", "APP");
     }
+
     @FXML
     private void openC(ActionEvent event) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("client-cards-screen.fxml"));
-        Parent root = fxmlLoader.load();
-        Scene scene = new Scene(root, 800, 500);
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setTitle("APP");
-        stage.setScene(scene);
-        stage.show();
+        changeScene(event, "client-cards-screen.fxml", "APP");
     }
+
     @FXML
     private void openD(ActionEvent event) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("purchases-card-facilitator.fxml"));
+        changeScene(event, "purchases-card-facilitator.fxml", "APP");
+    }
+
+    @FXML
+    private void exitApplication(ActionEvent event) {
+        Platform.exit();
+    }
+
+    private void changeScene(ActionEvent event, String fxmlFile, String title) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlFile));
         Parent root = fxmlLoader.load();
         Scene scene = new Scene(root, 800, 500);
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setTitle("APP");
+        stage.setTitle(title);
         stage.setScene(scene);
         stage.show();
     }
-
-
 
 }
