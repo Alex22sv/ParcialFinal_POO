ce7346442ec84b826f3965e75d7f54e9324e740d
Alex22sv <00024123@uca.edu.sv>
2024-07-09 01:27:12 -0600

    Save text files with .txt extension

--- ParcialFinal/src/main/java/org/alexsv/parcialfinal/Classes/Utilities.java
+++ ParcialFinal/src/main/java/org/alexsv/parcialfinal/Classes/Utilities.java
@@ -24,7 +24,7 @@ public class Utilities { // 00024123 Utility class which contains static methods
         LocalDateTime currentDateTime = LocalDateTime.now(); //00041923 get the local date (day, month, year and hour)
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"); //00041923 set the format of the day
         String formattedDateTime = currentDateTime.format(formatter); //00041923 convert the time to a String with the previous format
-        String nameFile = code +" " + formattedDateTime ; //00041923 the file name is a code with the current date
+        String nameFile = code +" " + formattedDateTime + ".txt"; //00041923 the file name is a code with the current date
         File newFile = new File(folderPath,nameFile); //00041923 make a new file in the folder path
         FileWriter writer = new FileWriter(newFile,false); // 00041923 make a writer to write the stream of characters to the text file
         writer.write(data);//00041923 write in file the data variable value
