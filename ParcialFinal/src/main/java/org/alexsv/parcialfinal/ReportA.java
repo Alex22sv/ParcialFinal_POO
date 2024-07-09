0a93505598d386aec50f6fab1b60e43789eb59ef
Aisack23 <00016023@uca.edu.sv>
2024-07-08 19:15:38 -0600

    Coments of the reports and the main-father controller

--- ParcialFinal/src/main/java/org/alexsv/parcialfinal/ReportA.java
+++ ParcialFinal/src/main/java/org/alexsv/parcialfinal/ReportA.java
@@ -1,49 +1,48 @@
 package org.alexsv.parcialfinal;
-
 import java.util.Date;
 
-public class ReportA {
-    private Date purchaseDate;
-    private Double totalAmount;
-    private String description;
-    private String cardNumber;
+public class ReportA { //00016023 - Defines the public class ReportA
+    private Date purchaseDate; //00016023 - Declares a private variable of type Date for the purchase date
+    private Double totalAmount; //00016023 - Declares a private variable of type Double for the total amount
+    private String description; //00016023 - Declares a private variable of type String for the description
+    private String cardNumber; //00016023 - Declares a private variable of type String for the card number
 
-    public ReportA(Date purchaseDate, Double totalAmount, String description, String cardNumber) {
-        this.purchaseDate = purchaseDate;
-        this.totalAmount = totalAmount;
-        this.description = description;
-        this.cardNumber = cardNumber;
+    public ReportA(Date purchaseDate, Double totalAmount, String description, String cardNumber) { //00016023 - Constructor of the ReportA class
+        this.purchaseDate = purchaseDate; //00016023 - Assigns the value of purchaseDate to the instance variable
+        this.totalAmount = totalAmount; //00016023 - Assigns the value of totalAmount to the instance variable
+        this.description = description; //00016023 - Assigns the value of description to the instance variable
+        this.cardNumber = cardNumber; //00016023 - Assigns the value of cardNumber to the instance variable
     }
 
-    public Date getPurchaseDate() {
-        return purchaseDate;
+    public Date getPurchaseDate() { //00016023 - Getter method for purchaseDate
+        return purchaseDate; //00016023 - Returns the value of purchaseDate
     }
 
-    public void setPurchaseDate(Date purchaseDate) {
-        this.purchaseDate = purchaseDate;
+    public void setPurchaseDate(Date purchaseDate) { //00016023 - Setter method for purchaseDate
+        this.purchaseDate = purchaseDate; //00016023 - Sets the value of purchaseDate
     }
 
-    public Double getTotalAmount() {
-        return totalAmount;
+    public Double getTotalAmount() { //00016023 - Getter method for totalAmount
+        return totalAmount; //00016023 - Returns the value of totalAmount
     }
 
-    public void setTotalAmount(Double totalAmount) {
-        this.totalAmount = totalAmount;
+    public void setTotalAmount(Double totalAmount) { //00016023 - Setter method for totalAmount
+        this.totalAmount = totalAmount; //00016023 - Sets the value of totalAmount
     }
 
-    public String getDescription() {
-        return description;
+    public String getDescription() { //00016023 - Getter method for description
+        return description; //00016023 - Returns the value of description
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setDescription(String description) { //00016023 - Setter method for description
+        this.description = description; //00016023 - Sets the value of description
     }
 
-    public String getCardNumber() {
-        return cardNumber;
+    public String getCardNumber() { //00016023 - Getter method for cardNumber
+        return cardNumber; //00016023 - Returns the value of cardNumber
     }
 
-    public void setCardNumber(String cardNumber) {
-        this.cardNumber = cardNumber;
+    public void setCardNumber(String cardNumber) { //00016023 - Setter method for cardNumber
+        this.cardNumber = cardNumber; //00016023 - Sets the value of cardNumber
     }
 }
--- ParcialFinal/src/main/java/org/alexsv/parcialfinal/ReportD.java
+++ ParcialFinal/src/main/java/org/alexsv/parcialfinal/ReportD.java
@@ -1,47 +1,47 @@
 package org.alexsv.parcialfinal;
 
-public class ReportD {
-        private Integer transactionId;
-    private String clientName;
-    private Integer totalPurchases;
-    private Double totalAmountSpent;
+public class ReportD { //00016023 - Defines the public class ReportD
+    private Integer transactionId; //00016023 - Declares a private variable of type Integer for the transaction ID
+    private String clientName; //00016023 - Declares a private variable of type String for the client name
+    private Integer totalPurchases; //00016023 - Declares a private variable of type Integer for the total purchases
+    private Double totalAmountSpent; //00016023 - Declares a private variable of type Double for the total amount spent
 
-    public ReportD(Integer transactionId, String clientName, Integer totalPurchases, Double totalAmountSpent) {
-        this.transactionId = transactionId;
-        this.clientName = clientName;
-        this.totalPurchases = totalPurchases;
-        this.totalAmountSpent = totalAmountSpent;
+    public ReportD(Integer transactionId, String clientName, Integer totalPurchases, Double totalAmountSpent) { //00016023 - Constructor of the ReportD class
+        this.transactionId = transactionId; //00016023 - Assigns the value of transactionId to the instance variable
+        this.clientName = clientName; //00016023 - Assigns the value of clientName to the instance variable
+        this.totalPurchases = totalPurchases; //00016023 - Assigns the value of totalPurchases to the instance variable
+        this.totalAmountSpent = totalAmountSpent; //00016023 - Assigns the value of totalAmountSpent to the instance variable
     }
 
-    public Integer getTransactionId() {
-        return transactionId;
+    public Integer getTransactionId() { //00016023 - Getter method for transactionId
+        return transactionId; //00016023 - Returns the value of transactionId
     }
 
-    public void setTransactionId(Integer transactionId) {
-        this.transactionId = transactionId;
+    public void setTransactionId(Integer transactionId) { //00016023 - Setter method for transactionId
+        this.transactionId = transactionId; //00016023 - Sets the value of transactionId
     }
 
-    public String getClientName() {
-        return clientName;
+    public String getClientName() { //00016023 - Getter method for clientName
+        return clientName; //00016023 - Returns the value of clientName
     }
 
-    public void setClientName(String clientName) {
-        this.clientName = clientName;
+    public void setClientName(String clientName) { //00016023 - Setter method for clientName
+        this.clientName = clientName; //00016023 - Sets the value of clientName
     }
 
-    public Integer getTotalPurchases() {
-        return totalPurchases;
+    public Integer getTotalPurchases() { //00016023 - Getter method for totalPurchases
+        return totalPurchases; //00016023 - Returns the value of totalPurchases
     }
 
-    public void setTotalPurchases(Integer totalPurchases) {
-        this.totalPurchases = totalPurchases;
+    public void setTotalPurchases(Integer totalPurchases) { //00016023 - Setter method for totalPurchases
+        this.totalPurchases = totalPurchases; //00016023 - Sets the value of totalPurchases
     }
 
-    public Double getTotalAmountSpent() {
-        return totalAmountSpent;
+    public Double getTotalAmountSpent() { //00016023 - Getter method for totalAmountSpent
+        return totalAmountSpent; //00016023 - Returns the value of totalAmountSpent
     }
 
-    public void setTotalAmountSpent(Double totalAmountSpent) {
-        this.totalAmountSpent = totalAmountSpent;
+    public void setTotalAmountSpent(Double totalAmountSpent) { //00016023 - Setter method for totalAmountSpent
+        this.totalAmountSpent = totalAmountSpent; //00016023 - Sets the value of totalAmountSpent
     }
 }
